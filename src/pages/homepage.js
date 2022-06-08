413bd9c3773c3fe16ba86ab7c2f785ffab45288f
Shlok Mehra <shlokmehra@rocketmail.com>
2022-06-08 23:49:44 +0530

    Styles Homepage

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -6,8 +6,15 @@ const Homepage = () => {
     console.log(quizDB)
     return(
         <>
-            <div className={`container center-block flex gap-xl justify-center light-border m3-top`} style = {{width: "50%"}}>
-                {renderQuizCard(quizDB)}
+            <div className="container center-block light-border m3-top" style = {{width: "50%"}}>
+                <div className="flex space-between">
+                    <p className="cursor-pointer active-category w-30 center-text">All</p>
+                    <p className="cursor-pointer border w-30 center-text">Marvel</p>
+                    <p className="cursor-pointer border w-30 center-text">DC</p>
+                </div>
+                <div className="flex gap-xl justify-center m3-top">
+                    {renderQuizCard(quizDB)}
+                </div>
             </div>
         </>
     )
