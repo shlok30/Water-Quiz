5941b886ea6cf43d980593f96f6cddf83921afca
Shlok Mehra <shlokmehra@rocketmail.com>
2022-06-08 23:06:47 +0530

    Feat : Added QuizCard Component

---
+++ src/pages/homepage.js
@@ -0,0 +1,12 @@
+
+const Homepage = () => {
+    return(
+        <>
+            <div className={`container center-block grid grid-3-columns gap-l light-border m3-top`}>
+                Quizes will come here
+            </div>
+        </>
+    )
+}
+
+export default Homepage
