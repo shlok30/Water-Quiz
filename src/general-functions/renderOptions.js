d77fc69fddb0717ef93ed292e1aee5cde5c189bb
Shlok Mehra <shlokmehra@rocketmail.com>
2022-06-10 17:07:14 +0530

    Refactor and adding key

--- src/general-functions/renderOptions.js
+++ src/general-functions/renderOptions.js
@@ -1,5 +1,5 @@
 import Options from "../components/options"
 
-const renderOptions = (currentQuizObject,options,index) => options.map((option) => <Options option = {option} index = {index} currentQuizObject = {currentQuizObject} />)
+const renderOptions = (currentQuizObject,options,index) => options.map((option) => <Options option = {option} index = {index} currentQuizObject = {currentQuizObject} key = {currentQuizObject["_id"]} />)
 
 export default renderOptions
